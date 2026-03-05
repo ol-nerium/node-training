867b63a0a032803aa0d0b74cfec821bc9bf038de
al-burlachenko <alexandrburl@gmail.com>
2026-03-05 15:32:52 +0200

    change settings to es modules

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const { generateRandomNumber, celciusToFarenheit } = require("./utils");
+import getPosts from "./postController.js";
+import { getPostsLength } from "./postController.js";
 
-console.log(`Random Number: ${generateRandomNumber()}`);
-console.log(`Celcius To Farenheit: ${celciusToFarenheit(0)}`);
+console.log(getPosts());
+console.log(getPostsLength());
+
+// const { generateRandomNumber, celciusToFarenheit } = require("./utils");
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celcius To Farenheit: ${celciusToFarenheit(0)}`);
