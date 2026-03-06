2ee12c7200c3d4c3b4dd07946d143658229e8c81
al-burlachenko <alexandrburl@gmail.com>
2026-03-06 19:46:53 +0200

    os demo

---
+++ osDemo.js
@@ -0,0 +1,14 @@
+// info about system
+import os, { freemem } from "os";
+
+// userInfo()
+console.log(os.userInfo());
+
+// totalmem()
+console.log(os.totalmem());
+
+//freemem()
+console.log(os.freemem());
+
+//cpus()
+console.log(os.cpus());
